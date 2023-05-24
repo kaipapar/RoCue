b9e8185b4a386773ff69dc34c6d29bcd2b6f3ae1
Karri Korsu <karri@DESKTOP-5US45FL>
2023-05-24 17:59:35 +0300

    update gitignore again

--- Documentation/html/search/all_1.js
+++ Documentation/html/search/all_1.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['position_0',['Position',['../struct_position.html',1,'']]]
+  ['floor_0',['Floor',['../struct_floor.html',1,'']]],
+  ['floorarraycreation_1',['floorArrayCreation',['../_floors_8c.html#a7dc2601f7d2a91e66e9f524f3c3a212e',1,'Floors.c']]],
+  ['floors_2ec_2',['Floors.c',['../_floors_8c.html',1,'']]]
 ];
